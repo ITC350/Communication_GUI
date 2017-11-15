4f452d05c84fd79e06818ad74742cf1949022580
Kasper Steensig <kasper.steensig@gmail.com>
2017-11-15 08:45:11 +0000

    try to receive data from hc-05

--- bt.py
+++ bt.py
@@ -3,14 +3,9 @@ import bluetooth
 max_data_size = 128
 
 def open_bt(mac_addr, port):
-    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM).connect((mac_addr, port))
-
-    try:
-        client, clientInfo = s.accept()
-        return (sock, client, client_info)
-
-    except:
-        return None
+    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    sock.connect((mac_addr, port))
+    return sock
 
 def close_bt(bt):
     bt.close()
@@ -21,4 +16,7 @@ def send_msg(bt, msg):
 def receive_msg(bt, msg, size = max_data_size):
     return bt.recv(size)
 
+sock = open_bt('98:D3:32:20:82:CA', 1)
+data = sock.recv(2)
 
+print(data)
