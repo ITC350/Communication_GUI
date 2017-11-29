3036b9a3c20054192bfc9005b6896690111c2dfd
Daniel Greth  Sørensen <dsaren16@student.aau.dk>
2017-11-29 12:25:06 +0100

    fixes

--- test.py
+++ test.py
@@ -9,17 +9,18 @@ start = 0x01
 settings = 0x02
 
 s = bytearray(struct.pack("b", settings)) + bytearray(struct.pack("I", 2)) + bytearray(struct.pack("I", 0)) + bytearray(struct.pack("I", 0)) + bytearray(struct.pack("I", 500)) + bytearray(struct.pack("I", 50)) + bytearray(struct.pack("I", 50)) + bytearray(struct.pack("b", start)) + bytearray(struct.pack("Q", 0))
+
 print(bytes(s))
 
 bt_dev.send_msg(bytes(s))
 
 msg = b''
 
-for i in range(0, 16):
+while True:
     msg = msg + bt_dev.receive_msg(127)
-
-msg = msg + bt_dev.receive_msg(16)
+    print(str(len(msg)) + ': ' + str(msg))
 
 print(str(msg))
+
 #msg = array.array('H', msg)
 #print(msg.tostring())
